29fed2112da582af8b8346581550723da300f58e
Dale Koch <dalek@marvin.com>
2022-04-24 21:07:08 -0500

    remove tx borders

--- remove-tv-borders.ps1
+++ remove-tv-borders.ps1
@@ -2,5 +2,5 @@ $files = Get-ChildItem -File -Filter *.png  -Path retroarch\overlay\GameBezels\S
 
 for ($i=0; $i -lt $files.Count; $i++) {
     Write-Output $files[$i].FullName
-    magick $files[$i].FullName -strokewidth 0 -fill black -draw "rectangle 222,0 1698,1080" -region "1477x1080+222+0" -transparent black $files[$i].FullName
+    magick $files[$i].FullName -strokewidth 0 -fill black -draw "rectangle 180,0 1735,1080" -region "1556x1080+180+0" -transparent black $files[$i].FullName
 }
